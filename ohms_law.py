721840e7cd9a5130adfae0da8d381ba5e05dbae4
Faith Wambui <wambuifaith2@gmail.com>
2025-08-11 04:20:44 +0300

    Added solution for Ohm's Law assignment

---
+++ ohms_law.py
@@ -0,0 +1,5 @@
+# Ohm's Law Calculator
+current = float(input("Enter the current (I) in amperes: "))
+resistance = float(input("Enter the resistance (R) in ohms: "))
+voltage = current * resistance
+print(f"The voltage (V) is: {voltage} volts")
